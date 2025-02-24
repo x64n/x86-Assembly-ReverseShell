98231cd9a6ed76fd98c06cde89dee2236ece4c2e
Claw <clkunz20@gmail.com>
2025-02-24 21:59:36 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,7 @@
 @echo off
+echo Setting up MSVC environment...
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+
 echo Compiling reverse_shell.asm...
 nasm -f win32 reverse_shell.asm -o reverse_shell.obj
 if errorlevel 1 goto error
